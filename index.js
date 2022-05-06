6f8fdf8e1fb6af4177251d6d8e744587815d9624
SlesSam <lesy.sam9422@gmail.com>
2022-05-06 09:30:57 +0200

    falta count y step

---
+++ index.js
@@ -0,0 +1,50 @@
+
+const ti =document.getElementById('numbers');
+var  counter =1;
+const step = parseInt(document.getElementById('step').value);
+
+
+//funcion expresada 
+const fu = (btn)=>{
+
+     if(btn.value=='start'){
+        counter = setInterval(()=>{
+            console.log(` empieza la cuenta  "${counter}" `),
+            timer()
+        }, document.getElementById('setTo').value);
+
+    }else if(btn.value=='pause'){
+        pause();
+        
+    }else if(btn.value=='reset'){
+        parseInt(document.getElementById('numbers').innerHTML);
+        document.getElementById('numbers').innerHTML = 0;
+        console.log("volvemos a 0 ")
+        pause();
+        
+    }
+// este quiero hacer que cuando le damos click al btn aunmente o disminuya el contador 
+    if(btn.value=='up'){
+        counter++;
+    }else if(btn.value=='down'){
+        counter--;
+    }
+
+}
+//me falta poner que el usuario me de los intervalos que vaya el contador.
+const timer=()=>{ 
+   const tio = parseInt(ti.innerHTML);
+   document.getElementById('numbers').innerHTML = eval(tio +2 );
+}
+
+const pause=()=>{
+    clearInterval(counter)
+    console.log("Se paro....... ")
+
+}
+
+//me faltaron dos para que este ejercicio este muy bien =(
+
+
+
+
